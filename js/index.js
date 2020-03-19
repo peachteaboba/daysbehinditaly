1963d1739172a4e790769dc47444c03c656d4fb4
Andy Feng <anndyfeng1@gmail.com>
2020-03-18 23:20:36 -0700

    new hotness

--- js/index.js
+++ js/index.js
@@ -1,41 +1,102 @@
+let dataRaw;
 let dataset = {};
 let dataCount = [];
+// const displayList = ['us', 'united_kingdom'];
 const displayList = [];
-const ignoreList = ['china', 'korea_south'];
-const minTotal = 400;
+const ignoreList = ['china', 'korea_south', 'cruise_ship'];
+const minTotal = 100;
+let startingCount = 500;
+let expand = false;
 
 (function init() {
-    getData();
+    $(document).ready(function () {
+        initControls();
 
+
+        getData();
+    });
 })();
 
+function initControls() {
+
+    // Set initial starting range from url param
+    const urlStarting = urlParams.get('starting') ? parseInt(urlParams.get('starting')) : 0;
+    if (urlStarting && urlStarting >= minTotal && urlStarting <= 10000) startingCount = urlStarting;
+
+    // Expand
+    if (urlParams.get('expand') && urlParams.get('expand') === '1') expand = true;
+
+    // Bind event handlers
+    $('#starting-range').mousemove(function () {
+        startingCount = $(this).val();
+        setSliders();
+    }).on('change', function () {
+        setTimeout(function () {
+            urlGenerator('starting', startingCount);
+            dataset = processData();
+            renderList();
+        }, 500);
+    }).val(startingCount);
+
+    $('#expand').on('change', function () {
+        expand = $(this).is(":checked");
+        urlGenerator('expand', expand ? '1' : '0');
+        dataset = processData();
+        renderList();
+    });
+
+    // Set initial values
+    setSliders();
+}
+
+function setSliders() {
+    // Starting
+    $('#starting-range-label').text(startingCount);
+
+    // Expand
+    $('#expand').prop('checked', expand);
+}
+
+function urlGenerator(field, value) {
+    let searchParams = new URLSearchParams(window.location.search);
+    searchParams.set(field, value);
+    const newParams = searchParams.toString();
+    let newUrl = location.href.split('?')[0];
+    history.pushState('', 'Days Behind Italy - COVID19', newUrl += '?' + newParams);
+}
+
 function getData() {
-    $.get( "https://pomber.github.io/covid19/timeseries.json", function( data ) {
-        dataset = processData(data);
+    $.get("https://pomber.github.io/covid19/timeseries.json", function (data) {
+        dataRaw = data;
+        dataset = processData();
+    }).then(function () {
         renderList();
     });
 }
 
-function processData(obj) {
+function processData() {
+    dataCount = [];
+    dataset = [];
+    let obj = dataRaw;
     let processed = {};
-    for (var prop in obj) {
+    for (let prop in obj) {
         if (Object.prototype.hasOwnProperty.call(obj, prop)) {
             // Remove under 100 cases
             let arr = [];
             let latest = 0;
             if (obj[prop] && obj[prop].length > 0) {
-                for (let i=0; i<obj[prop].length; i++) {
-                    if (obj[prop][i].confirmed >= 100) {
+                for (let i = 0; i < obj[prop].length; i++) {
+                    if (obj[prop][i]['confirmed'] >= startingCount) {
                         arr.push(obj[prop][i]);
-                        if (i === obj[prop].length -1) {
-                            latest = obj[prop][i].confirmed;
+                        if (i === obj[prop].length - 1) {
+                            latest = obj[prop][i]['confirmed'];
                         }
                     }
                 }
             }
             const id = prop.toLowerCase().replace(' ', '_').replace(',', '');
             if (arr.length > 0 && latest >= minTotal) {
-                if (displayList.length > 0 && displayList.indexOf(id) === -1) {
+                if (displayList.length > 0 && displayList.indexOf(id) === -1 && id !== 'italy') {
                     // Skip this
                 } else if (ignoreList.indexOf(id) === -1) {
                     processed[id] = {
@@ -43,7 +104,7 @@ function processData(obj) {
                         data: arr,
                         count: arr.length,
                         latest: latest
-                    }
+                    };
                     dataCount.push({
                         id: id,
                         count: arr.length,
@@ -53,7 +114,7 @@ function processData(obj) {
             }
         }
     }
-    dataCount.sort((a, b) => (a.latest < b.latest) ? 1 : -1)
+    dataCount.sort((a, b) => (a.latest < b.latest) ? 1 : -1);
     return processed;
 }
 
@@ -61,44 +122,55 @@ function renderList() {
     console.log(dataset);
     console.log(dataCount);
 
+    const bodyEl = $('#body');
+    bodyEl.html('');
 
-    // Render day column
-    const dayWrapper = $('#day-wrapper');
-    dayWrapper.html(render.dayColumn(dataset.italy.count));
-
-    // Render italy
-    const italyWrapper = $('#italy-wrapper');
-    italyWrapper.html(render.countryColumn(dataset.italy));
-
+    let html_arr = [];
+    dataCount.forEach(function (el) {
+        if (el.id !== 'italy' && dataset[el.id] && dataset[el.id].data) {
+            //const country = dataset[id];
 
 
+            // Create body wrapper div
+            html_arr.push([
+                '<div class="body-wrapper">',
+
+                // Render day element
+                '<div class="day-wrapper">',
+                render.dayColumn(dataset['italy'].count),
+                '</div>',
+                // Render italy element
+                '<div class="italy-wrapper">',
+                render.countryColumn(dataset['italy']),
+                '</div>',
+                // Render country element
+                '<div class="country-wrapper">',
+                render.countryColumn(dataset[el.id]),
+                '</div>',
 
 
+                '</div>'
+            ].join(''));
+
+
+        }
+    });
+
+    bodyEl.html(html_arr.join(''));
+
+
+    // // Render day column
+    // const dayWrapper = $('#day-wrapper');
+    // dayWrapper.html();
+    //
+    // // Render italy
+    // const italyWrapper = $('#italy-wrapper');
+    // italyWrapper.html(render.countryColumn(dataset['italy']));
+    //
+    //
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // end
--- js/render.js
+++ js/render.js
@@ -1,77 +1,90 @@
-var render = {};
+const urlParams = new URLSearchParams(window.location.search);
+let render = {};
 
-render.dayColumn = function(count) {
+render.dayColumn = function (count) {
     let html = [
         '<div class="day-title">Day</div>'
     ];
-    for (let i=1; i <= count + 10; i++) {
+    for (let i = 1; i <= count; i++) {
         const dynamicClass = i % 2 !== 0 ? 'even' : '';
-        html.push('<div class="'+dynamicClass+'"> ' + i + ' </div>');
+        html.push('<div class="' + dynamicClass + '"> ' + i + ' </div>');
     }
     return html.join('');
-}
+};
 
-render.countryColumn = function(data) {
-
-    console.log(data);
-
+render.countryColumn = function (data) {
     let body = ['<div class="country-body">'];
 
-    // Render Date
-    body.push('<div class="country-date">');
-    body.push('<div class="country-date-title">Date</div>');
-    for (let i=0; i <= data.data.length - 1; i++) {
-        console.log(data.data[i]);
-        let date = new Date(data.data[i].date);
-        date = moment(date).format('ddd, MMM D');
-        const dynamicClass = i % 2 === 0 ? 'date even' : 'date';
-        body.push('<div class="'+dynamicClass+'"> ' + date + ' </div>');
-    }
-    body.push('</div>');
-
-    // Render Cases
-    body.push('<div class="country-date">');
-    body.push('<div class="country-date-title">Cases</div>');
-    for (let i=0; i <= data.data.length - 1; i++) {
-        const dynamicClass = i % 2 === 0 ? 'cases even' : 'cases';
-        body.push('<div class="'+dynamicClass+'"> ' + data.data[i].confirmed + ' </div>');
-    }
-    body.push('</div>');
-
-
-    // TODO: Make this toggleable ::::::::::::
+    // Build Date HTML :::::::::::::::::::::::
     // :::::::::::::::::::::::::::::::::::::::
-    // Render Deaths
-    body.push('<div class="country-date">');
-    body.push('<div class="country-date-title">Deaths</div>');
-    for (let i=0; i <= data.data.length - 1; i++) {
-        const dynamicClass = i % 2 === 0 ? 'cases even' : 'cases';
-        body.push('<div class="'+dynamicClass+'"> ' + data.data[i].deaths + ' </div>');
+    let date_html = [];
+    date_html.push('<div class="country-date">');
+    date_html.push('<div class="country-date-title">Date</div>');
+    for (let i = 0; i <= data.data.length - 1; i++) {
+        //console.log(data.data[i]);
+        let date = new Date(data.data[i]['date']);
+        date = moment(date).format('ddd, MMM DD');
+        const dynamicClass = i % 2 === 0 ? 'date even' : 'date';
+        date_html.push('<div class="' + dynamicClass + '"> ' + date + ' </div>');
     }
-    body.push('</div>');
-    // Render Recovered
-    body.push('<div class="country-date">');
-    body.push('<div class="country-date-title">Rec</div>');
-    for (let i=0; i <= data.data.length - 1; i++) {
-        const dynamicClass = i % 2 === 0 ? 'cases even' : 'cases';
-        body.push('<div class="'+dynamicClass+'"> ' + data.data[i].recovered + ' </div>');
+    date_html.push('</div>');
+
+    // Build Cases HTML ::::::::::::::::::::::
+    // :::::::::::::::::::::::::::::::::::::::
+    let cases_html = [];
+    cases_html.push('<div class="country-date">');
+    cases_html.push('<div class="country-date-title">Cases</div>');
+    for (let i = 0; i <= data['data'].length - 1; i++) {
+        const dynamicClass = i % 2 === 0 ? 'cases total even' : 'cases total';
+        cases_html.push('<div class="' + dynamicClass + '"> ' + data.data[i]['confirmed'] + ' </div>');
     }
+    cases_html.push('</div>');
+
+    // Build Deaths / Rec HTML :::::::::::::::
+    // :::::::::::::::::::::::::::::::::::::::
+    let deaths_html = [];
+    let rec_html = [];
+    //const urlParams = new URLSearchParams(window.location.search);
+    if (expand) {
+        // Render Deaths
+        deaths_html.push('<div class="country-date">');
+        deaths_html.push('<div class="country-date-title">Deaths</div>');
+        for (let i = 0; i <= data['data'].length - 1; i++) {
+            const dynamicClass = i % 2 === 0 ? 'cases deaths even ' : 'cases deaths';
+            deaths_html.push('<div class="' + dynamicClass + '"> ' + data.data[i]['deaths'] + ' </div>');
+        }
+        deaths_html.push('</div>');
+        // Render Recovered
+        rec_html.push('<div class="country-date">');
+        rec_html.push('<div class="country-date-title">Rec</div>');
+        for (let i = 0; i <= data.data.length - 1; i++) {
+            const dynamicClass = i % 2 === 0 ? 'cases rec even' : 'cases rec';
+            rec_html.push('<div class="' + dynamicClass + '"> ' + data.data[i]['recovered'] + ' </div>');
+        }
+        rec_html.push('</div>');
+    }
+
+    // Combine components ::::::::::::::::::::
+    // :::::::::::::::::::::::::::::::::::::::
+    if (data['name'] === 'Italy') {
+        body.push(date_html.join(''));
+        body.push(rec_html.join(''));
+        body.push(deaths_html.join(''));
+        body.push(cases_html.join(''));
+    } else {
+        body.push(cases_html.join(''));
+        body.push(deaths_html.join(''));
+        body.push(rec_html.join(''));
+        body.push(date_html.join(''));
+    }
+
     body.push('</div>');
-    // ::::::::::::::::::::::::::::::::::::::
-    // ::::::::::::::::::::::::::::::::::::::
-
-
-
-
-    body.push('</div>');
-
-
 
     let html = [
-        '<div class="country-title"><h1>'+data.name+'</h1><p>P - 60M</p></div>',
+        '<div class="country-title"><h1>' + data.name + '</h1><p>P · 60M</p></div>',
         body.join('')
-    ]
+    ];
 
     return html.join('');
 
-}
+};
